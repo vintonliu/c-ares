62c923a839da4f03433acc5a03376caf27e62f93
liuwenchang <liuwenchang@51talk.com>
2020-11-23 10:47:58 +0800

    update ios build script.

--- build_ios.sh
+++ build_ios.sh
@@ -24,19 +24,26 @@ INSTALL_DIR="$CWD/out/ios/install/$BUILD_MODE"
 THIN="$INSTALL_DIR"
 FAT="$INSTALL_DIR/all"
 rm -rf $INSTALL_DIR
-# rm -rf $FAT
 
 if [ -n "$1" -a "$1" == "clean" ]; then
     rm -rf $OBJECT_DIR
     exit 0
 fi
 
-# must add -DANDROID=1, else could not generate Android CMakeFiles
-PLATFORM_CONFIG="-DCMAKE_TOOLCHAIN_FILE=$CWD/cmake/ios.toolchain.cmake \
-               -DCMAKE_BUILD_TYPE=$BUILD_MODE \
-               -DENABLE_BITCODE=0 \
-               -DENABLE_ARC=0 \
-               -DDEPLOYMENT_TARGET=10.0"
+# PLATFORM_CONFIG="-DCMAKE_TOOLCHAIN_FILE=$CWD/cmake/ios.toolchain.cmake \
+#                -DCMAKE_BUILD_TYPE=$BUILD_MODE \
+#                -DENABLE_BITCODE=0 \
+#                -DENABLE_ARC=0 \
+#                -DDEPLOYMENT_TARGET=9.0"
+
+PLATFORM_CONFIG="-DCMAKE_BUILD_TYPE=$BUILD_MODE \
+                -DCMAKE_SYSTEM_NAME=iOS"
+
+CARES_CONFIG="-DCARES_STATIC=True \
+            -DCARES_STATIC_PIC=True \
+            -DCARES_SHARED=False \
+            -DCARES_BUILD_TOOLS=False \
+            -DCARES_BUILD_TESTS=False"
 
 PLATFORMS=(
     "OS"
@@ -44,26 +51,47 @@ PLATFORMS=(
     "SIMULATOR64"
 )
 
+ARCHS=(
+    "armv7;armv7s;arm64"
+    "i386"
+    "x86_64"
+)
+
+IOSFLAGS="-miphoneos-version-min=9.0"
+
 build_cares() {
-    CFLAGS="-DCARES_USE_LIBRESOLV"
+    # CFLAGS="-DCARES_USE_LIBRESOLV $IOSFLAGS"
+    CFLAGS="$IOSFLAGS"
 
     num=${#PLATFORMS[@]}
     for((i=0; i<num; i++))
     do
+        echo "************** building for ${PLATFORMS[i]} *************"
         if [ ! -d "$OBJECT_DIR/${PLATFORMS[i]}" ]
         then
             mkdir -p "$OBJECT_DIR/${PLATFORMS[i]}"
         fi
         cd "$OBJECT_DIR/${PLATFORMS[i]}"
 
-        CMAKE_CONFIG="$PLATFORM_CONFIG \
-                    -DCARES_STATIC=True \
-                    -DCARES_STATIC_PIC=True \
-                    -DCARES_SHARED=False \
-                    -DCARES_BUILD_TOOLS=False \
-                    -DCARES_BUILD_TESTS=False \
-                    -DCMAKE_C_FLAGS=$CFLAGS \
-                    -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR/${PLATFORMS[i]}"
+        if [ "${PLATFORMS[i]}" = "SIMULATOR" -o "${PLATFORMS[i]}" = "SIMULATOR64" ]
+        then
+            PLATFORM="iPhoneSimulator"
+        else
+            PLATFORM="iPhoneOS"
+        fi
+
+        SYSROOT="$(xcode-select -print-path)/Platforms/$PLATFORM.platform/Developer/SDKs/$PLATFORM.sdk/"
+        CC=`xcrun -sdk ${SYSROOT} -find clang`
+        CXX=`xcrun -sdk ${SYSROOT} -find clang++`
+
+        CMAKE_CONFIG="$CARES_CONFIG \
+                    $PLATFORM_CONFIG \
+                    -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR/${PLATFORMS[i]} \
+                    -DCMAKE_OSX_ARCHITECTURES=${ARCHS[i]} \
+                    -DCMAKE_OSX_SYSROOT=$SYSROOT \
+                    -DCMAKE_C_COMPILER=$CC \
+                    -DCMAKE_CXX_COMPILER=$CXX \
+                    -DCMAKE_C_FLAGS=$CFLAGS"
 
         cmake -G"Ninja" $CMAKE_CONFIG -DPLATFORM=${PLATFORMS[i]} $SOURCE_PATH || exit 1
         ninja -j8 install || exit 1
